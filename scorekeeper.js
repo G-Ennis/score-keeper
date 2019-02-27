6291b68bed3d0de382571761c1a05d468fd76523
Gulcin Ennis <gulcin.ennis@gmail.com>
2019-02-26 22:27:41 -0500

    Set a max score click  for each player using boolean var, then end game

--- scorekeeper.js
+++ scorekeeper.js
@@ -4,12 +4,24 @@ var p1Display = document.getElementById("p1Display");
 var p2Display = document.getElementById("p2Display");
 var p1Score = 0; // variable initially set to 0, will be incremented with each "click"
 var p2Score = 0;
+var gameOver = false; // gameOver variable will be true when a player reaches the winning score
+var winningScore = 5;
 
 p1Button.addEventListener("click", function(){
+    if (!gameOver){
     p1Score++;
-    p1Display.textContent = p1Score;
-})
+    if (p1Score === winningScore){
+        gameOver = true;
+    }
+    p1Display.textContent = p1Score;   
+    }
+});
 p2Button.addEventListener("click", function(){
+    if(!gameOver){
     p2Score++;
-    p2Display.textContent = p2Score;
-})
+    if (p2Score === winningScore){
+        gameOver = true;
+    }
+    p2Display.textContent = p2Score;    
+    }
+});
